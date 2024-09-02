8a7c4782d14ab5a660409e9da44cf1f5ce32a31d
onticinc <onticinc@gmail.com>
2024-09-02 16:44:32 -0600

    built login page and layout for auth folder

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/LoginButton";
 
 const font = Poppins({ 
   subsets:["latin"], 
@@ -13,15 +14,15 @@ export default function Home() {
   return (
    <>
    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,var(--tw-gradient-stops))] from-sky-400 to-blue-800 ">
-      <div className="space-y-6">
-        <h1 className={cn("text-6xl font-semibold text-white drop-shadow-md text-center", font.className,)}>🔐 Auth</h1>
-        <p className="text-white text-lg text-center">A simple authentication service</p>
-        <div className="flex space-x-4">
-          <Button variant="secondary" size="lg">Sign In</Button>
-        </div>
-      
+      <div className="space-y-6 text-center">
+        <h1 className={cn("text-6xl font-semibold text-white drop-shadow-md", font.className,)}>🔐 Auth</h1>
+        <p className="text-white text-lg">A simple authentication service</p>
+        
+        <LoginButton>
+          <Button  variant="secondary" size="lg">Sign In</Button>
+        </LoginButton>
       </div>
    </main>
-   </>
+   </> 
   );
 }
