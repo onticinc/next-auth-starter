065a0bdfe8e40f084b9d768f5e2ec9ead2f879c2
onticinc <onticinc@gmail.com>
2024-09-11 16:22:20 -0600

    setting up middleware

--- auth.ts
+++ auth.ts
@@ -1,13 +1,18 @@
-import NextAuth from "next-auth"
-import Github from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 
+import { db } from "@/lib/db";
+import authConfig from "@/auth.config";
 
 export const {
     handlers: { GET, POST},
     auth,
 }  = NextAuth({
-    providers: [Github]
+    adapter: PrismaAdapter(db),
+    session: { strategy: "jwt" },
+    ...authConfig,
 });
 
 
 
+ 
