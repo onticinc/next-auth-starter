47ded9fa90685292b3acb71b98b31a3ed0fbd06e
onticinc <onticinc@gmail.com>
2024-09-10 18:09:29 -0600

    setting up next auth"

---
+++ auth.ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth"
+import Github from "next-auth/providers/github"
+
+
+export const {
+    handlers: { GET, POST},
+    auth,
+}  = NextAuth({
+    providers: [Github]
+});
+
+
+
