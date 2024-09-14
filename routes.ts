ea0915061fdd39220bde6959a34a5d23a23f016d
onticinc <onticinc@gmail.com>
2024-09-13 22:01:38 -0600

    debugging problem with infinite loop in auth system

--- routes.ts
+++ routes.ts
@@ -1,7 +1,48 @@
 /**
  * Array of public routes
+ * These routes do not require Authentication
+ * @type {string[]}
  */
 
 export const publicRoutes = [
-    "/"
+    "/",
+    
 ];
+
+
+
+
+/**
+ * An Array of routes that are used for authentication.
+ * These routes will redirect logged in users to /settings
+ * @type {string[]}
+ */
+
+export const authRoutes = [
+    "auth/login", 
+    "auth/register",
+];
+
+
+
+
+/**
+ * The prefix for API authentication routes
+ * Routes that start with this prefix are used for API authentication purposes
+ * @type {string}
+ */
+
+export const apiAuthPrefix = "/api/auth";
+
+
+
+
+
+
+/**
+ * The default redirect path after loggin in. 
+ * Routes that start with this prefix are used for API authentication purposes
+ * @type {string}
+ */
+
+export const DEFAULT_LOGIN_REDIRECT = "/";
