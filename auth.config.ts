f4b1102482fb7656f2cfb83ba544b8bd0e582568
onticinc <onticinc@gmail.com>
2024-10-01 15:51:27 -0600

    got google login and register working

--- auth.config.ts
+++ auth.config.ts
@@ -1,13 +1,29 @@
 import bcrypt from "bcryptjs";
 import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+
 import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import Linkedin from "next-auth/providers/linkedin";
 
 import { LoginSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
 
 export default {
     providers: [
+        Linkedin({
+            clientId: process.env.LINKEDIN_ID,
+            clientSecret: process.env.LINKEDIN_SECRET
+        }),
+
+        Github({
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        }),
+        Google({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
         Credentials({
             async authorize(credentials) {
                 const validatedFields = LoginSchema.safeParse(credentials);
--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,13 +1,21 @@
 'use client';
 
+import { signIn } from "next-auth/react";
+
 import { FcGoogle} from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 
 export const Social = () => {
+    const onClick = (provider: "google" | "github" | "facebook" | "linkedin") => {
+        signIn(provider, { 
+            callbackUrl: DEFAULT_LOGIN_REDIRECT 
+        }); 
+    }
     return (
         <div className="flex flex-col w-full items-center">
             <div className="mt-2">
@@ -15,7 +23,7 @@ export const Social = () => {
                     size="lg"
                     className="min-w-80"
                     variant="outline"
-                    onClick={() => {}}
+                    onClick={() => onClick("google")}
                 >
                     <FcGoogle  className="h-5 w-5" />
                     <span className="pl-1"> Google</span>
@@ -26,7 +34,7 @@ export const Social = () => {
                     size="lg"
                     className="min-w-80"
                     variant="outline"
-                    onClick={() => {}}
+                    onClick={() => onClick("linkedin")}
                 >
                     <FaLinkedin  className="h-5 w-5" />
                     <span className="pl-1"> LinkedIn</span>
@@ -37,7 +45,7 @@ export const Social = () => {
                     size="lg"
                     className="min-w-80"
                     variant="outline"
-                    onClick={() => {}}
+                    onClick={() => onClick("github")}
                 >
                     <FaGithub  className="h-5 w-5" />
                     <span className="pl-1"> Github</span>
@@ -48,14 +56,12 @@ export const Social = () => {
                     size="lg"
                     className="min-w-80"
                     variant="outline"
-                    onClick={() => {}}
+                    onClick={() => onClick("facebook")}
                 >
                     <FaFacebook  className="h-5 w-5" />
                     <span className="pl-1"> Facebook</span>
                 </Button>
             </div>
-
-
         </div>
     );
     }
